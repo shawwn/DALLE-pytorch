d653b2925e744e67389cdb223160789aa3f9d6df
Hannu Töyrylä <hannu.toyryla@gmail.com>
2021-01-09 12:18:08 +0200

    update trainVAE

--- trainVAE.py
+++ trainVAE.py
@@ -3,6 +3,7 @@ from torch import nn, optim
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 from dalle_pytorch import DiscreteVAE
 
 imgSize = 256
@@ -10,50 +11,68 @@ imgSize = 256
 vae = DiscreteVAE(
     image_size = imgSize,
     num_layers = 3,
-    num_tokens = 1024,
-    codebook_dim = 512,
-    hidden_dim = 64
+    channels = 3,
+    num_tokens = 2048,
+    codebook_dim = 1024,
+    hidden_dim = 128
 )
 
-batchSize = 64
-n_epochs = 100
-log_interval = 20
+vae.cuda()
+
+batchSize = 48
+n_epochs = 500
+log_interval = 10
 #images = torch.randn(4, 3, 256, 256)
 
 t = transforms.Compose([
   transforms.Resize(imgSize),
   transforms.RandomHorizontalFlip(),
-  transforms.ToTensor()
-  #transforms.Normalize((0.368, 0.324, 0.318), (0.5, 0.5, 0.5)) #(0.267, 0.233, 0.234))
+  transforms.ToTensor(),
+  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #(0.267, 0.233, 0.234))
   ])
 
 train_set = datasets.ImageFolder('./imagedata', transform=t, target_transform=None)
 
 train_loader = DataLoader(dataset=train_set, num_workers=1, batch_size=batchSize, shuffle=True)
 
-optimizer = optim.Adam(vae.parameters(), lr=1e-4)
+optimizer = optim.Adam(vae.parameters(), lr=1e-5)
+#optimizer.cuda()
 
 # train with a lot of data to learn a good codebook
 for epoch in range(0, n_epochs):
 
     train_loss = 0
     for batch_idx, (images, _) in enumerate(train_loader):
-        loss = vae(images, return_recon_loss = True)
-        loss.backward()
+        images = images.cuda()
+        #loss = vae(images, return_recon_loss = True)
+        #loss.backward()
+        recons = vae(images)
+        loss = F.smooth_l1_loss(images, recons) 
+        
 
+        optimizer.zero_grad()
+        loss.backward()
         train_loss += loss.item()
         optimizer.step()
 
-        if batch_idx % 1 == 0:
+        if batch_idx % log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(images), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
                 loss.item() / len(images)))
 
-    sample = vae(images, return_recon_loss = False)
-    save_image(sample.view(-1, 3, imgSize, imgSize),
-               'results/sample_' + str(epoch) + '.png')
+    k = 8
+    with torch.no_grad():
+        codes = vae.get_codebook_indices(images)
+        imgx = vae.decode(codes)
+    grid = torch.cat([images[:k], recons[:k], imgx[:k]])
+    save_image(grid,
+               'results/epoch_' + str(epoch) + '.png', normalize=True)
+
     print('====> Epoch: {} Average loss: {:.4f}'.format(
           epoch, train_loss / len(train_loader.dataset)))
+
+    torch.save(vae.state_dict(), "./models/dvae-"+str(epoch)+".pth")
+
 
     
