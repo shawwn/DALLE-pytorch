e6084762b21fb2eac35c7fea422cd59c0a4e702f
Hannu Töyrylä <hannu.toyryla@gmail.com>
2021-01-11 10:33:44 +0200

    traindalle: continue training, added comments

--- trainDALLE.py
+++ trainDALLE.py
@@ -5,15 +5,29 @@ import torchvision.transforms as transforms
 import torch.optim as optim
 from torchvision.utils import save_image
 
+# vae
+
 load_epoch = 390
 vaename = "vae-cdim256"
+
+# general
 
 imgSize = 256
 batchSize = 12
 n_epochs = 100
 log_interval = 10
-lr = 1e-4
+lr = 2e-5
+
+#dalle
+
+# to continue training from a saved checkpoint, give checkpoint path as loadfn and start_epoch 
+
+#loadfn = "./models/dalle_vae-cdim256-140.pth"   
+#start_epoch = 140
+loadfn = ""
+start_epoch = 0
 name = "vae-cdim256"
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -33,6 +47,8 @@ vae = DiscreteVAE(
     temperature = 0.9
 )
 
+# load pretrained vae
+
 vae_dict = torch.load("./models/"+vaename+"-"+str(load_epoch)+".pth")
 vae.load_state_dict(vae_dict)
 vae.to(device)
@@ -49,11 +65,19 @@ dalle = DALLE(
     ff_dropout = 0.1            # feedforward dropout
 )
 
+
+# load pretrained dalle if continuing training
+if loadfn != "":
+    dalle_dict = torch.load(loadfn)
+    dalle.load_state_dict(dalle_dict)
+
 dalle.to(device)
 
 # get image and text data
 
-lf = open("od-captionsonly.txt", "r")
+lf = open("od-captionsonly.txt", "r")   # file contains captions only, one caption per line
+
+# build vocabulary
 
 from Vocabulary import Vocabulary
 
@@ -68,8 +92,9 @@ for caption in captions:
     
 def tokenizer(text): # create a tokenizer function
     return text.split(' ')
-    
-lf = open("od-captions.txt", "r")
+
+
+lf = open("od-captions.txt", "r") # files contains lines in the format image_path : captions
 
 data = []
 
@@ -89,7 +114,9 @@ for lin in lf:
 
 len_data = len(data)
 print(len_data)
-datactr = 0
+#datactr = 0
+
+# an iterator for fetching data during training
 
 class ImageCaptions:
     
@@ -121,33 +148,36 @@ class ImageCaptions:
                 break     
         return i_data, c_data
 
+
 optimizer = optim.Adam(dalle.parameters(), lr=lr)
 
-for epoch in range(0, n_epochs):
+for epoch in range(start_epoch, start_epoch+n_epochs):
   batch_idx = 0    
   train_loss = 0    
-  dset = ImageCaptions(data, batchsize=batchSize)
-  for i,c in dset:
-    #print(i, c)
-    text = torch.LongTensor(c)
-    images = torch.zeros(len(i), 3, 256, 256)
+  dset = ImageCaptions(data, batchsize=batchSize) # initialize iterator
+  
+  for i,c in dset:  # loop through dataset by minibatch
+    text = torch.LongTensor(c)  # a minibatch of text (numerical tokens)
+    images = torch.zeros(len(i), 3, 256, 256) # placeholder for images
     
     text = text.to(device)
     #print(text)
     
+    # fetch images into tensor based on paths given in minibatch
     ix = 0
-    for imgfn in i:
-        #print(imgfn)
-        img_t = read_image("./imagedata/0/"+imgfn).float()/255.
-        img_t = tf(img_t)
-        #print(img_t.shape, img_t.mean())
-        images[ix,:,:,:] = img_t
+    for imgfn in i:       # iterate through image paths in minibatch
+
+        # note: images are expected to be in ./imagefolder/0/
+        img_t = read_image("./imagedata/0/"+imgfn).float()/255.   # read image and scale into float 0..1
+        img_t = tf(img_t)  # normalize 
+        images[ix,:,:,:] = img_t 
         ix += 1
 
     images = images.to(device)
         
     mask = torch.ones_like(text).bool().to(device)
     
+    # train and optimize a single minibatch
     optimizer.zero_grad()
     loss = dalle(text, images, mask = mask, return_loss = True)
     train_loss += loss.item()
@@ -167,8 +197,8 @@ for epoch in range(0, n_epochs):
 
   torch.save(dalle.state_dict(), "./models/dalle_"+name+"-"+str(epoch)+".pth")
   
+  # generate a test sample from the captions in the last minibatch
   oimgs = dalle.generate_images(text, mask = mask)
-  #print(epoch, ) # (2, 3, 256, 256)
   save_image(oimgs,
                'results/dalle'+name+'_epoch_' + str(epoch) + '.png', normalize=True)
 
