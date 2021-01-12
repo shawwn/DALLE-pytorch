dd99ddcd2e371808b505f2844471e1194eb53297
Hannu Töyrylä <hannu.toyryla@gmail.com>
2021-01-12 11:32:52 +0200

    cmd line args in trainVAE

--- trainVAE.py
+++ trainVAE.py
@@ -1,3 +1,4 @@
+import argparse
 import torch
 from torch import nn, optim
 from torchvision import datasets, transforms
@@ -7,20 +8,33 @@ import torch.nn.functional as F
 from dalle_pytorch import DiscreteVAE
 from torch.nn.utils import clip_grad_norm_
 
-imgSize = 256
-batchSize = 24
-n_epochs = 500
-log_interval = 10
-lr = 1e-4
-temperature_scheduling = True
+parser = argparse.ArgumentParser(description='train VAE for DALLE-pytorch')
+parser.add_argument('--batchSize', type=int, default=24, help='batch size for training (default: 24)')
+parser.add_argument('--dataPath', type=str, default="./imagedata", help='path to imageFolder (default: ./imagedata')
+parser.add_argument('--imageSize', type=int, default=256, help='image size for training (default: 256)')
+parser.add_argument('--n_epochs', type=int, default=500, help='number of epochs (default: 500)')
+parser.add_argument('--lr', type=float, default=1e-4, help='learning rate (default: 1e-4)')
+parser.add_argument('--tempsched', action='store_true', default=False, help='use temperature scheduling')
+parser.add_argument('--temperature', type=float, default=0.9, help='vae temperature (default: 0.9)')
+parser.add_argument('--name', type=str, default="vae", help='experiment name')
+parser.add_argument('--loadVAE', type=str, default="", help='name for pretrained VAE when continuing training')
+parser.add_argument('--start_epoch', type=int, default=0, help='start epoch numbering for continuing training (default: 0)')
+opt = parser.parse_args()
 
-name = "v2vae256"
+imgSize = opt.imageSize #256
+batchSize = opt.batchSize #24
+n_epochs = opt.n_epochs #500
+log_interval = 10
+lr = opt.lr #1e-4
+temperature_scheduling = opt.tempsched #True
+
+name = opt.name #"v2vae256"
 
 # for continuing training 
 # set loadfn: path to pretrained model
 # start_epoch: start epoch numbering from this
-loadfn = ""
-start_epoch = 0
+loadfn = opt.loadVAE #""
+start_epoch = opt.start_epoch #0
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -30,7 +44,8 @@ vae = DiscreteVAE(
     channels = 3,
     num_tokens = 2048,
     codebook_dim = 1024,
-    hidden_dim = 128
+    hidden_dim = 128,
+    temperature = opt.temperature
 )
 
 if loadfn != "":
@@ -46,14 +61,14 @@ t = transforms.Compose([
   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #(0.267, 0.233, 0.234))
   ])
 
-train_set = datasets.ImageFolder('./imagedata', transform=t, target_transform=None)
+train_set = datasets.ImageFolder(opt.dataPath, transform=t, target_transform=None)
 
 train_loader = DataLoader(dataset=train_set, num_workers=1, batch_size=batchSize, shuffle=True)
 
 optimizer = optim.Adam(vae.parameters(), lr=lr)
 
 if temperature_scheduling:
-    vae.temperature = 5
+    vae.temperature = opt.temperature
     dk = 0.7 ** (1/len(train_loader)) 
     print('Scale Factor:', dk)
 
