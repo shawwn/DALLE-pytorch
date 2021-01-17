ed9b1e110bf6148a983c8e20716f1f7571edcffe
Shawn Presser <shawnpresser@gmail.com>
2021-01-16 23:20:55 -0800

    CenterCropLongEdge

--- trainVAE.py
+++ trainVAE.py
@@ -56,10 +56,59 @@ if loadfn != "":
 
 vae.to(device)
 
+
+class CenterCropLongEdge(object):
+  """Crops the given PIL Image on the long edge.
+  Args:
+      size (sequence or int): Desired output size of the crop. If size is an
+          int instead of sequence like (h, w), a square crop (size, size) is
+          made.
+  """
+  def __call__(self, img):
+    """
+    Args:
+        img (PIL Image): Image to be cropped.
+    Returns:
+        PIL Image: Cropped image.
+    """
+    return transforms.functional.center_crop(img, min(img.size))
+
+  def __repr__(self):
+    return self.__class__.__name__
+
+
+
+class RandomCropLongEdge(object):
+  """Crops the given PIL Image on the long edge with a random start point.
+  Args:
+      size (sequence or int): Desired output size of the crop. If size is an
+          int instead of sequence like (h, w), a square crop (size, size) is
+          made.
+  """
+  def __call__(self, img):
+    """
+    Args:
+        img (PIL Image): Image to be cropped.
+    Returns:
+        PIL Image: Cropped image.
+    """
+    size = (min(img.size), min(img.size))
+    # Only step forward along this edge if it's the long edge
+    i = (0 if size[0] == img.size[0]
+          else np.random.randint(low=0,high=img.size[0] - size[0]))
+    j = (0 if size[1] == img.size[1]
+          else np.random.randint(low=0,high=img.size[1] - size[1]))
+    return transforms.functional.crop(img, i, j, size[0], size[1])
+
+  def __repr__(self):
+    return self.__class__.__name__
+
+
 t = transforms.Compose([
+  CenterCropLongEdge(),
   transforms.Resize(imgSize),
   transforms.ToTensor(),
-  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #(0.267, 0.233, 0.234))
+  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #(0.267, 0.233, 0.234)
   ])
 
 train_set = datasets.ImageFolder(opt.dataPath, transform=t, target_transform=None)
