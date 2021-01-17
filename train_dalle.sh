4d65798b59611a0d870dbdb6d9bf1a5fe96a25b4
Shawn Presser <shawnpresser@gmail.com>
2021-01-17 02:27:03 -0800

    Add train_dalle.sh

--- train_dalle.sh
+++ train_dalle.sh
@@ -1,6 +1,7 @@
 set -ex
 export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-1}"
+vae_epoch="${vae_epoch:-30}"
 
 mkdir -p results models
 
-exec python3 -m pdb -c continue trainDALLE.py --dataPath download --vae_epoch 30 "$@"
+exec python3 -m pdb -c continue trainDALLE.py --dataPath download --vae_epoch "$vae_epoch" "$@"
